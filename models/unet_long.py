9edb64477c53dfc6874a108bc61f8cc6b363e5df
Long Christian <long.vo210404@vnuk.edu.vn>
2024-05-19 14:01:43 +0700

    Add LayerNorm

--- models/unet_long.py
+++ models/unet_long.py
@@ -6,7 +6,7 @@ from .involution import Involution_CUDA
 import torchvision.transforms.functional as TF
 
 
-
+# Will do this later
 class RMSNorm(nn.Module):
     def __init__(self, dim):
         super().__init__()
@@ -17,23 +17,43 @@ class RMSNorm(nn.Module):
         # Normalize across the channel dimension
         return F.normalize(x, dim=1) * self.scale * self.gamma.view(1, -1, 1, 1)
     
+
     
-class LayerNorm2d(nn.Module):
-    def __init__(self, num_channels, eps=1e-5):
-        super(LayerNorm2d, self).__init__()
-        self.layer_norm = nn.LayerNorm([num_channels, 1, 1], eps=eps)
+class LayerNorm(nn.Module):
+    """ LayerNorm that supports two data formats: channels_last (default) or channels_first. 
+    The ordering of the dimensions in the inputs. channels_last corresponds to inputs with 
+    shape (batch_size, height, width, channels) while channels_first corresponds to inputs 
+    with shape (batch_size, channels, height, width).
+
+    Source: https://github.com/facebookresearch/ConvNeXt-V2/blob/main/models/utils.py
+    """
+    def __init__(self, normalized_shape, eps = 1e-6, data_format = "channels_last"):
+        super().__init__()
+
+        self.weight = nn.Parameter(torch.ones(normalized_shape))
+        self.bias = nn.Parameter(torch.zeros(normalized_shape))
+        self.eps = eps
+        self.data_format = data_format
+
+        if self.data_format not in ["channels_last", "channels_first"]:
+            raise NotImplementedError
+
+        self.normalized_shape = (normalized_shape, )
+
 
     def forward(self, x):
-        # Reshape to (batch_size, num_channels, height * width)
-        batch_size, num_channels, height, width = x.size()
-        x_reshaped = x.view(batch_size, num_channels, -1)
-        
-        # Apply LayerNorm and then reshape back
-        x_norm = self.layer_norm(x_reshaped)
-        x_norm = x_norm.view(batch_size, num_channels, height, width)
-        
-        return x_norm
 
+        if self.data_format == "channels_last":
+            return F.layer_norm(x, self.normalized_shape, self.weight, 
+                                self.bias, self.eps)
+        elif self.data_format == "channels_first":
+            u = x.mean(1, keepdim = True)
+            s = (x - u).pow(2).mean(1, keepdim = True)
+            x = (x - u) / torch.sqrt(s + self.eps)
+
+            x = self.weight[:, None, None] * x + self.bias[:, None, None]
+
+            return x
     
 
 class BottleNeck_Long(nn.Module):
@@ -41,7 +61,7 @@ class BottleNeck_Long(nn.Module):
         super(BottleNeck_Long, self).__init__()
         self.invblock = nn.Sequential(
             Involution_CUDA(in_channels, kernel_size = inv_kernel, stride = 1),
-            nn.BatchNorm2d(in_channels),
+            LayerNorm(in_channels),
             nn.GELU()
         )
 
@@ -52,7 +72,7 @@ class BottleNeck_Long(nn.Module):
                       stride = 1,
                       padding = 0,
                       bias = False),
-            nn.BatchNorm2d(out_channels),
+            LayerNorm(out_channels),
         )   
 
         self.mapping=nn.Sequential(
@@ -60,7 +80,7 @@ class BottleNeck_Long(nn.Module):
                       out_channels = out_channels,
                       kernel_size = 1,
                       padding = 0),
-            nn.BatchNorm2d(out_channels))
+            LayerNorm(out_channels))
         
         self.gelu = nn.GELU()
 
