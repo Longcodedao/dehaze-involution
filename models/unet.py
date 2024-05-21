28761df5da3a1d3df0f3235c4abcfdb98abca002
root <long.vo210404@vnuk.edu.vn>
2024-05-21 02:18:33 +0000

    Send code

--- models/unet.py
+++ models/unet.py
@@ -5,6 +5,44 @@ from .involution import Involution_CUDA, Involution
 
 import torchvision.transforms.functional as TF
 
+
+class LayerNorm(nn.Module):
+    """ LayerNorm that supports two data formats: channels_last (default) or channels_first. 
+    The ordering of the dimensions in the inputs. channels_last corresponds to inputs with 
+    shape (batch_size, height, width, channels) while channels_first corresponds to inputs 
+    with shape (batch_size, channels, height, width).
+
+    Source: https://github.com/facebookresearch/ConvNeXt-V2/blob/main/models/utils.py
+    """
+    def __init__(self, normalized_shape, eps = 1e-6, data_format = "channels_last"):
+        super().__init__()
+
+        self.weight = nn.Parameter(torch.ones(normalized_shape))
+        self.bias = nn.Parameter(torch.zeros(normalized_shape))
+        self.eps = eps
+        self.data_format = data_format
+
+        if self.data_format not in ["channels_last", "channels_first"]:
+            raise NotImplementedError
+
+        self.normalized_shape = (normalized_shape, )
+
+
+    def forward(self, x):
+
+        if self.data_format == "channels_last":
+            return F.layer_norm(x, self.normalized_shape, self.weight, 
+                                self.bias, self.eps)
+        elif self.data_format == "channels_first":
+            u = x.mean(1, keepdim = True)
+            s = (x - u).pow(2).mean(1, keepdim = True)
+            x = (x - u) / torch.sqrt(s + self.eps)
+
+            x = self.weight[:, None, None] * x + self.bias[:, None, None]
+
+            return x
+
+
 class UNET_block(nn.Module):
     def __init__(self, in_channels, out_channels, use_cuda = True):
         super(UNET_block,self).__init__()
@@ -23,7 +61,7 @@ class UNET_block(nn.Module):
             
         self.involution = nn.Sequential(
             self.involution_block(out_channels, kernel_size = 7, stride = 1),
-            nn.BatchNorm2d(out_channels),
+            LayerNorm(out_channels, data_format="channels_first"),
             nn.GELU())
         
         self.convblock2 = nn.Sequential(
@@ -33,7 +71,7 @@ class UNET_block(nn.Module):
                       stride = 1,
                       padding = 0,
                       bias = False),
-            nn.BatchNorm2d(out_channels))
+            LayerNorm(out_channels, data_format = "channels_first"))
         
         self.gelu = nn.GELU()
         
@@ -42,7 +80,7 @@ class UNET_block(nn.Module):
                       out_channels = out_channels,
                       kernel_size = 1,
                       padding = 0),
-            nn.BatchNorm2d(out_channels))
+            LayerNorm(out_channels, data_format = "channels_first"))
 
     
     def forward(self,x):
@@ -95,7 +133,7 @@ class UNET(nn.Module):
             # Instead of using AvgPool2D
             self.downs.append(nn.Conv2d(in_channels = in_channels_temp,
                                      out_channels = in_channels_temp,
-                                     kernel_size = 1,
+                                     kernel_size = 2,
                                      stride = 2,
                                      padding = 0))
 
